ca23444d9caa88fe9636b099786dc89b90012f10
Sam <sgusick@uvm.edu>
2023-01-25 09:21:59 -0500

    finished many links

--- generate.py
+++ generate.py
@@ -1,5 +1,22 @@
 import pyrosim.pyrosim as pyrosim
 
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = .5
+
+
 pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+for gridx in range(5):
+    for gridy in range(5):
+        for a in range(10):
+            scale = pow(.9, a)
+            pyrosim.Send_Cube(name="Box", pos=[gridx,gridy,z+a] , size=[scale * length, scale * width, scale * height])
+
+# pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length, width, height])
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
 import pybullet as p
 import time
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+planeId = p.loadURDF("plane.urdf")
+
+p.setGravity(0,0,-9.8)
 p.loadSDF("box.sdf")
-
 
 for x in range(1000):
     p.stepSimulation()
