6a4d61c47d178cb1848c4cca77c724c13553f494
Sam <sgusick@uvm.edu>
2023-03-06 16:42:04 -0500

    random search done

--- constants.py
+++ constants.py
@@ -9,7 +9,6 @@ phase_offset_back_leg = 0
 amplitude_front_leg = pi/4
 frequency_front_leg = 5
 phase_offset_front_leg = 1
-program_run_time = 10000
+program_run_time = 1000
 gravity = -9.8
 sleep_time = 1/240
-
--- generate.py
+++ generate.py
@@ -1,5 +1,5 @@
 import pyrosim.pyrosim as pyrosim
-
+import random
 length = 1
 width = 1
 height = 1
@@ -32,15 +32,26 @@ def Generate_Body():
 
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
-    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
-    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
+
+
+    pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso"),
+    pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg"),
     pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
+
+
+
+    # pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0 )
+    # pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -1.0 )
+
+    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg"),
     pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -1.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0 )
-    pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -1.0 )
+
+    for i in range(3):
+        for j in range(3,5):
+                pyrosim.Send_Synapse( sourceNeuronName = i , targetNeuronName = j , weight =  random.uniform(-1.0, 1.0))
+
     pyrosim.End()
 
 Generate_Body()
